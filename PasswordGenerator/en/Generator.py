61b89212c6e0d32be81476b76d8b8b1d2b7725a0
Antonin <antoninpicard0@gmail.com>
2023-02-22 16:02:18 +0100

    Add ENGLISH language

--- PasswordGenerator/en/Generator.py
+++ PasswordGenerator/en/Generator.py
@@ -4,73 +4,75 @@ import string
 def RandomPS():
     caracteres = string.ascii_letters + string.digits 
     password = ''.join(random.choices(caracteres, k=n)) 
-    print("Votre mot de passe est : ", password)
+    print("Your password is: ", password)
 
-# Demander à l'utilisateur la longueur souhaitée du mot de passe
-n = int(input("Entrez le nombre de caractères que vous souhaitez dans votre mot de passe : "))
+# Ask the user for the desired password length
+n = int(input("Enter the number of characters you want in your password: "))
 nb = n
 
-# Demander à l'utilisateur le type de mot de passe souhaité
-password_type = input("Entrez le type de mot de passe souhaité (L pour les lettres, N pour les chiffres, B pour les deux) : ")
+# Ask the user for the desired password type
+password_type = input("Enter the desired password type (L for letters, N for numbers, B for both): ")
 
-# Si l'utilisateur choisit un mot de passe de lettres
+# If the user chooses a password of letters
 if password_type == "L":
     n_letters = n
     n_numbers = 0
     characters = string.ascii_letters
 
-# Si l'utilisateur choisit un mot de passe de chiffres
+# If the user chooses a password of numbers
 elif password_type == "N":
     n_numbers = n
     n_letters = 0
     characters = string.digits
 
-# Si l'utilisateur choisit un mot de passe de lettres et de chiffres
+# If the user chooses a password of letters and numbers
 elif password_type == "B":
-    randomRep = input("Voulez-vous générer un mot de passe avec des lettres et des chiffres aléatoires ? (O pour Oui ou N pour Non)")
+    randomRep = input("Do you want to generate a password with random letters and numbers ? (Y for Yes or N for No)")
     if randomRep == "O":
         RandomPS()
-    # Demander à l'utilisateur le nombre de lettres souhaité
-    n_letters = int(input("Entrez le nombre de lettres que vous souhaitez dans votre mot de passe : "))
+    # Ask the user for the desired number of letters
+    n_letters = int(input("Enter the number of letters you want in your password: "))
     
     while n_letters > n or n_letters < 0:
-        print("ERREUR le nombre de lettres ne peut pas être supérieur à la longueur de mot de passe choisie.")
-        response = input("Voulez-vous changer le nombre de caractères de votre mot de passe ? (O pour Oui ou N pour Non)")
-        if response == "O":
-            n = int(input("Entrez le nombre de caractères que vous souhaitez dans votre mot de passe : "))
+        print("ERROR the number of letters cannot be greater than the chosen password length.")
+        response = input("Do you want to change the number of characters in your password? (Y for Yes or N for No)")
+        if response == "Y":
+            n = int(input("Enter the number of characters you want in your password: "))
         else:
-            n_letters = int(input("Entrez le nombre de lettres que vous souhaitez dans votre mot de passe : "))
+            n_letters = int(input("Enter the number of letters you want in your password: "))
             
-    # Demander à l'utilisateur le nombre de chiffres souhaité
+    # Ask the user for the desired number of numbers
     nb = nb - n_letters
-    n_numbers = int(input("Entrez le nombre de chiffres que vous souhaitez dans votre mot de passe : "))
+    n_numbers = int(input("Enter the number of numbers you want in your password: "))
     
     while n_numbers > n or n_numbers > nb or n_numbers < 0:
-        print("ERREUR le nombre de lettres et de chiffres ne peut pas être supérieur à la longueur de mot de passe choisie.")
-        response = input("Voulez-vous changer le nombre de caractères de votre mot de passe ? (O pour Oui ou N pour Non)")
-        if response == "O":
-            n = int(input("Entrez le nombre de caractères que vous souhaitez dans votre mot de passe : "))
+        print("ERROR the number of letters and numbers cannot be greater than the chosen password length.")
+        response = input("Do you want to change the number of characters in your password? (Y for Yes or N for No)")
+        if response == "Y":
+            n = int(input("Enter the number of characters you want in your password: "))
         else:
-            n_numbers = int(input("Entrez le nombre de chiffres que vous souhaitez dans votre mot de passe : "))
+            n_numbers = int(input("Enter the number of numbers you want in your password: "))
     
     characters = string.ascii_letters + string.digits
 
 else:
-    print("Type de mot de passe invalide. Veuillez entrer L pour les lettres, N pour les chiffres ou B pour les deux.")
+    print("Invalid password type. Please enter L for letters, N for numbers, or B for both.")
 
-# Générer le mot de passe
+# Generate the password
 password = ''.join(random.choices(characters, k=n_letters + n_numbers))
-# Insérer les lettres
+
+# Insert the letters
 random_letters = random.choices(string.ascii_letters, k=n_letters)
 for i in range(n_letters):
     index = random.randint(0, n - 1)
     password = password[:index] + random_letters[i] + password[index+1:]
 
-# Insérer les nombres
+# Insert the numbers
 random_numbers = random.choices(string.digits, k=n_numbers)
 for i in range(n_numbers):
     index = random.randint(0, n - 1)
     password = password[:index] + random_numbers[i] + password[index+1:]
 
-# Afficher le mot de passe
-print("Votre mot de passe est : ", password)
+# Display the password
+print("Your password is: ", password)
+
